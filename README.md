9c3cdd27789b133113160606104d580db164e0e6
feluishercules <fernando_hercules9@hotmail.com>
2021-08-24 00:21:23 -0600

    Primera parte del taller #2 de precios y descuentos

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 - Segundo paso: Implementar las formulas en Javascript 
 - Tercer paso: crear funciones 
 - Cuarto paso: integrar Javascript con HTML
+
+
+## Taller #2: Porcentajes y Descuentos
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en Javascript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
---
+++ descuentos.js
@@ -0,0 +1,14 @@
+// const precioOriginal = 120;
+// const descuento =  18;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+
+// console.log(`El precio original es ${precioOriginal}$`)
+
