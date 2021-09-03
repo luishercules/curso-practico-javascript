5a45c097c646613170379e43178e000db9e6fb9e
feluishercules <fernando_hercules9@hotmail.com>
2021-09-02 23:55:38 -0600

    calculando la moda, convirtiendola a funcion

---
+++ moda.js
@@ -0,0 +1,60 @@
+// const lista1 = [
+// 1,
+// 2,
+// 3,
+// 1,
+// 2,
+// 3,
+// 4,
+// 2,
+// 2,
+// 2,
+// 1,
+// ];
+
+// const lista1Count = {
+// };
+
+// lista1.map(
+//     function (elemento) {
+//         if (lista1Count[elemento]) {
+//             lista1Count[elemento] +=1;
+//         }else{
+//             lista1Count[elemento] = 1;
+//         }
+//     }
+// );
+
+// const lista1Array = Object.entries(lista1Count).sort(
+//     function (elementoA, elementoB) {
+//         return elementoA[1]-elementoB[1];
+//     }
+// );
+
+// const moda = lista1Array[lista1Array.length -1];
+
+
+
+
+//CONVERTIR EN FUNCION
+function moda(lista) {
+    const listaCount = {}
+     
+      lista.map(function (elemento) {
+    if (listaCount[elemento]) {
+          listaCount[elemento] += 1
+        }else {
+          listaCount[elemento] = 1
+        }
+      })
+    
+    const listaArray = Object.entries(listaCount).sort(function (
+        elementoA,
+        elementoB
+      ) {
+    return elementoA[1] - elementoB[1]
+      })
+    
+    const moda = listaArray[listaArray.length - 1]
+      console.log(moda)
+    }
